4023c5fee8361d632ac2138ddef74e37e3dcf9ad
João Merlin <88944432+Merlin262@users.noreply.github.com>
2022-08-11 20:41:01 -0300

    Add files via upload

---
+++ TDE 1 - 6.py
@@ -0,0 +1,7 @@
+cateto1=float(input("Informe o valor do primeiro cateto"))
+cateto2=float(input("Informe o valor do segundo cateto"))
+
+hipotenusaq=(cateto1**2)+(cateto2**2)
+hipotenusa=hipotenusaq**(1/2)
+
+print("O valor da hipotenusa equivale a " + str(hipotenusa))

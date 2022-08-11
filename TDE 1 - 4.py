942abf1564adafc2261529540afd11c9782e9873
João Merlin <88944432+Merlin262@users.noreply.github.com>
2022-08-10 22:13:58 -0300

    Add files via upload

---
+++ TDE 1 - 4.py
@@ -0,0 +1,8 @@
+from cmath import pi
+
+
+diametro=float(input("Informe o diametro"))
+
+areaCircunferencia=pi*((diametro/2)**2)
+
+print("A area da circunferencia sera de " + str(areaCircunferencia))

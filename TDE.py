d98d06260acbd69c556ef15f784011f90a6aed58
João Merlin <88944432+Merlin262@users.noreply.github.com>
2022-08-10 21:41:41 -0300

    Add files via upload

---
+++ TDE.py
@@ -0,0 +1,6 @@
+numero=int(input("Digite um numero"))
+
+numeroFinal1=(numero-1)
+numeroFinal2=(numero+1)
+
+print("Seu antecessor eh " + str(numeroFinal1) + "e seu sucessor eh " + str(numeroFinal2))
